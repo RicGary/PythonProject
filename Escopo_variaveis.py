65f3afd871577f2be80c8c09caf5e98624a34ebd
EricNaiber <ericnaiber@gmail.com>
2021-04-12 20:16:01 -0300

    Aulas

---
+++ Escopo_variaveis.py
@@ -0,0 +1,19 @@
+'''
+Escopo das variáveis.
+
+1-Variáveis globais:
+    -São reconhecidas por todo o programa.
+
+2-Variáveis locais:
+    -São reconhecidas no bloco em que foi declarada.
+
+Em python não colocamos o seu tipo em determinada variável;
+Exemplo C:
+int numero = 42;
+
+Exemplo Java:
+int numero = 42;
+
+Exemplo Python:
+numero = 42
+'''
---
+++ Estruturas_condicionais.py
@@ -0,0 +1,21 @@
+"""
+Estudo de condicionais
+if, else, elif
+and, or:binários
+
+not, is:uniários
+"""
+
+ativo = True
+logado = True
+
+if ativo and logado:            #Os dois precisam ser True para rodar
+    print('Bem-vindo usuário.')
+
+if ativo or logado:             #Apenas 1 precisa ser True
+    print('Bem-vindo.')
+
+if not aivo:                    #not inverte os valores True->False
+    print('Você precisa ativar sua conta.')
+
+if
